351ea592dd74f821917f3310bfa1e1deb8963b74
Edith Toubi <edithtoubi@MacBook-Air-de-Edith.local>
2022-07-09 20:41:01 +0200

    Updates

---
+++ precache-manifest.044eafd6a91447e2fc60496257f09668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83b1c24c98d736c09ad2",
+    "url": "/react-project/static/css/main.b8737963.chunk.css"
+  },
+  {
+    "revision": "83b1c24c98d736c09ad2",
+    "url": "/react-project/static/js/main.83b1c24c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-project/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/react-project/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "58aba666dc7454bb3086",
+    "url": "/react-project/static/js/runtime~main.58aba666.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "878d99c6d18e57087da530ec668dfcb3",
+    "url": "/react-project/index.html"
+  }
+];

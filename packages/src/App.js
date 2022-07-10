81b5d1369d896ddbd20cf63322ff7202324d7de7
edithbdev <edith.toubi@gmail.com>
2022-07-10 18:22:11 +0200

    fix file workflow

--- packages/src/App.js
+++ packages/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>{`Hello ${process.env.REACT_APP_NAME}`} fggfgf</p>
+        <p>Hello there</p>
         <a
           className="App-link"
           href="https://reactjs.org"
